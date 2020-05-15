491db5c42eab0529d567b62e8d9b109a203c0d38
Mateusz Szafraniec <mateusz@mashkor.com>
2020-05-15 17:22:03 +0200

    After lesson 293.

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../providers/great_places.dart';
 import '../screens/add_place_screen.dart';
 
 class PlacesListScreen extends StatelessWidget {
@@ -14,8 +16,24 @@ class PlacesListScreen extends StatelessWidget {
           },
         ),
       ]),
-      body: Center(
-        child: CircularProgressIndicator(),
+      body: Consumer<GreatPlaces>(
+        child: Center(
+          child: const Text('No places yet, start adding some'),
+        ),
+        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+            ? ch
+            : ListView.builder(
+                itemCount: greatPlaces.items.length,
+                itemBuilder: (ctx, i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: FileImage(greatPlaces.items[i].image),
+                  ),
+                  title: Text(greatPlaces.items[i].title),
+                  onTap: () {
+                    // Go to details page
+                  },
+                ),
+              ),
       ),
     );
   }
--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,13 +1,38 @@
-import 'package:flutter/material.dart';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspath;
+
 class ImageInput extends StatefulWidget {
+  final Function onSelectImage;
+
+  ImageInput(this.onSelectImage);
+
   @override
   _ImageInputState createState() => _ImageInputState();
 }
 
 class _ImageInputState extends State<ImageInput> {
   File _storedImage;
+
+  Future<void> _takePicture() async {
+    final imageFile = await ImagePicker.pickImage(
+      source: ImageSource.gallery,
+      maxHeight: 600,
+    );
+    if (imageFile == null) {
+      return;
+    }
+    setState(() {
+      _storedImage = imageFile;
+    });
+    final appDir = await syspath.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await imageFile.copy('${appDir.path}/$fileName');
+    widget.onSelectImage(savedImage);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,9 +51,12 @@ class _ImageInputState extends State<ImageInput> {
                   width: double.infinity,
                 )
               : Padding(
-                padding: const EdgeInsets.all(2),
-                child: Text('No image taken', textAlign: TextAlign.center,),
-              ),
+                  padding: const EdgeInsets.all(2),
+                  child: Text(
+                    'No image taken',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
           alignment: Alignment.center,
         ),
         SizedBox(
@@ -39,7 +67,7 @@ class _ImageInputState extends State<ImageInput> {
             icon: Icon(Icons.camera),
             label: Text('Take picture'),
             textColor: Theme.of(context).primaryColor,
-            onPressed: () {},
+            onPressed: _takePicture,
           ),
         ),
       ],
