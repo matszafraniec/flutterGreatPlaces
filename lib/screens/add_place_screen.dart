f53c5d4a34823c9f6fcc36e17503185aeed26a50
Mateusz Szafraniec <mat.szafraniec@outlook.com>
2020-05-16 18:37:38 +0200

    added static map snapshot

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../widgets/location_input.dart';
 import '../widgets/image_input.dart';
 import '../providers/great_places.dart';
 
@@ -52,6 +53,8 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                     ),
                     SizedBox(height: 10),
                     ImageInput(_selectImage),
+                    SizedBox(height: 10),
+                    LocationInput(),
                   ],
                 ),
               ),
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -16,23 +16,31 @@ class PlacesListScreen extends StatelessWidget {
           },
         ),
       ]),
-      body: Consumer<GreatPlaces>(
-        child: Center(
-          child: const Text('No places yet, start adding some'),
-        ),
-        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
-            ? ch
-            : ListView.builder(
-                itemCount: greatPlaces.items.length,
-                itemBuilder: (ctx, i) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(greatPlaces.items[i].image),
-                  ),
-                  title: Text(greatPlaces.items[i].title),
-                  onTap: () {
-                    // Go to details page
-                  },
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(child: CircularProgressIndicator() )
+            : Consumer<GreatPlaces>(
+                child: Center(
+                  child: const Text('No places yet, start adding some'),
                 ),
+                builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+                    ? ch
+                    : ListView.builder(
+                        itemCount: greatPlaces.items.length,
+                        itemBuilder: (ctx, i) => ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage:
+                                FileImage(greatPlaces.items[i].image),
+                          ),
+                          title: Text(greatPlaces.items[i].title),
+                          onTap: () {
+                            // Go to details page
+                          },
+                        ),
+                      ),
               ),
       ),
     );
